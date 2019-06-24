96ea91c19a83ee97eb0e581e63ab0a572f5f719c
Monse0051 <rouge0051@gmail.com>
2019-06-24 09:03:23 -0500

    Updates

---
+++ precache-manifest.7fe82da0e6cdbf3e23fc921d45769abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7895866a097d6e290099040b439cf8eb",
+    "url": "/GDL002-framework-social-network/index.html"
+  },
+  {
+    "revision": "0ce6091869b00d13c749",
+    "url": "/GDL002-framework-social-network/static/css/main.baa7caae.chunk.css"
+  },
+  {
+    "revision": "18058acec3609a11cff2",
+    "url": "/GDL002-framework-social-network/static/js/2.8f1c57c0.chunk.js"
+  },
+  {
+    "revision": "0ce6091869b00d13c749",
+    "url": "/GDL002-framework-social-network/static/js/main.5a8b7bc2.chunk.js"
+  },
+  {
+    "revision": "6cead313ad965984df26",
+    "url": "/GDL002-framework-social-network/static/js/runtime~main.33323341.js"
+  }
+]);
